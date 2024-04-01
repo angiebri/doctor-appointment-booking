16e1a15c0d26063fd35e5fd692733be867d92622
Angelina Briginets <ani-bri@ya.ru>
2024-04-01 18:55:21 +0300

    my first commit

---
+++ app/_components/CategorySeach.jsx
@@ -0,0 +1,62 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import GlobalApi from "../_utils/GlobalApi";
+import Image from "next/image";
+
+function CategorySeach() {
+  const [categoryList, setCategotyList] = useState([]);
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then((resp) => {
+      console.log(resp.data.data);
+      setCategotyList(resp.data.data);
+    });
+  };
+  return (
+    <div className="mb-10 items-center px-5 flex flex-col gap-2">
+      <h2 className="font-bold text-4xl tracking-wide">
+        Search <span className="text-primary">Doctors</span>
+      </h2>
+      <h2 className="text-gray-500 text-xl">
+        Search Your Doctor and Book Appointment in one click
+      </h2>
+
+      <div className="flex w-full mt-3 max-w-sm items-center space-x-2">
+        <Input type="text" placeholder="Search..." />
+        <Button type="submit">
+          <Search className="h-4 w-4 mr-2" />
+          Search
+        </Button>
+      </div>
+      <div className="grid grid-cols-3 mt-5 md:grid-cols-6 lg:grid-col-6">
+        {categoryList.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col text-center items-center p-5 bg-teal-50 m-2 rounded-lg cursor-pointer gap-2 hover:scale-110 transition-all ease-in-out"
+          >
+            <Image
+              src={
+                process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
+                item.attributes?.Icon?.data.attributes?.url
+              }
+              unoptimized={true}
+              alt="icon"
+              width={40}
+              height={40}
+            />
+            <label className="text-teal-600 text-sm">
+              {item?.attributes?.Name}
+            </label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CategorySeach;
