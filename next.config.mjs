b583f7c1770801af3832dd4c8ee9c32829a7a89c
Angelina Briginets <ani-bri@ya.ru>
2024-04-12 16:32:30 +0500

    Deploy Strapi on Server render.com

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["localhost", "lh3.googleusercontent.com"],
+    domains: [
+      "localhost",
+      "lh3.googleusercontent.com",
+      "doctor-book-admin.onrender.com",
+    ],
   },
 };
 
