45186b014431073019983da31970887971b8cfc9
Angelina Briginets <ani-bri@ya.ru>
2024-04-19 01:26:56 +0500

    changed const baseUrl = process.env.NEXT_PUBLIC_DOMAIN

--- emails/index.jsx
+++ emails/index.jsx
@@ -13,8 +13,8 @@ import {
 import * as React from "react";
 import moment from "moment";
 
-const baseUrl = process.env.DOMAIN
-  ? `https://${process.env.DOMAIN}`
+const baseUrl = process.env.NEXT_PUBLIC_DOMAIN
+  ? `https://${process.env.NEXT_PUBLIC_DOMAIN}`
   : "http://localhost:3000";
 
 export const EmailTemplate = ({ UserName, Time, Date, doctorName }) => {
